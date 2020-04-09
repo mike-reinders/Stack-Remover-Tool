486c4faf4e1c9bffbfcdb62b13896831c25e7d9c
Mike Reinders <mike.reinders.gg@gmail.com>
2020-04-09 16:05:05 +0200

    redundant public modifier

--- src/reinders/mike/StackRemoverTool/ClassMapping/MappingInterface.java
+++ src/reinders/mike/StackRemoverTool/ClassMapping/MappingInterface.java
@@ -4,11 +4,11 @@ import java.util.Map;
 
 public interface MappingInterface {
 
-    public String getName();
+    String getName();
 
-    public Map<String, String> getMapping();
+    Map<String, String> getMapping();
 
-    public default String getReplacement(String origin) {
+    default String getReplacement(String origin) {
         for (Map.Entry<String, String> entry : this.getMapping().entrySet()) {
             if (entry.getKey().equals(origin)) {
                 return entry.getValue();
@@ -18,7 +18,7 @@ public interface MappingInterface {
         return null;
     }
 
-    public default String getOrigin(String replacement) {
+    default String getOrigin(String replacement) {
         for (Map.Entry<String, String> entry : this.getMapping().entrySet()) {
             if (entry.getValue().equals(replacement)) {
                 return entry.getKey();
